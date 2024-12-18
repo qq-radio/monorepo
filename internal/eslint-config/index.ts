44ff337bfff5555f2535b901b7dcfc88eab6b084
qq-radio <qq_radio@163.com>
2024-12-18 16:40:34 +0800

    make it shit make it right make it fast

---
+++ internal/eslint-config/index.ts
@@ -0,0 +1,44 @@
+export default {
+  parser: 'vue-eslint-parser',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['vue', '@typescript-eslint', 'import'],
+  rules: {
+    'no-undef': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        args: 'all',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'vue/attributes-order': ['error'],
+    'vue/multi-word-component-names': ['warn'],
+  },
+}
